abf549933c902fe803112c27782dc2b78046b90c
Nikhil Wagh <nikhil.wagh@bell.ca>
2020-04-17 16:52:02 +0530

    Initial commit.

---
+++ TelestraDemoApp/TelestraDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelestraDemoApp
+//
+//  Created by Nikhil Wagh on 17/04/20.
+//  Copyright © 2020 Nikhil Wagh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
