b5b95b805a6cf1930436f527ef7fab21b29523a5
Nikhil Wagh <NW00594382@techmahindra.com>
2020-04-20 20:49:39 +0530

    Final code commit.

---
+++ TelstraDemoApp/TelstraDemoApp/Utility/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  TelstraDemoApp
+//
+//  Created by Nikhil Wagh on 18/04/20.
+//  Copyright © 2020 Tech Mahindra. All rights reserved.
+//
+
+import Foundation
+
+struct Constant {
+    
+    struct API {
+        static let factsURL = "https:dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+    }
+    
+    static let cellIdentifier = "FactsCell"
+    static let errorTitle = "Error"
+    static let ok = "OK"
+    static let customFontBold = "Helvetica-bold"
+    static let customFont = "Helvetica"
+    static let titleFontSize = 18.0
+    static let descFontSize = 16.0
+    static let imageViewHeight = 90.0
+    static let imageViewWidth = 90.0
+    static let zero = 0.0
+    static let heightConstant = 60.0
+    
+}
